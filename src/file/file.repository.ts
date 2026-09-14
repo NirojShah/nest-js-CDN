662883f833400d163268c7f1301f10195eb9ceec
Niroj Shah <nirojshah1998@gmail.com>
2026-09-14 23:23:41 +0530

    Implemet the repository functions -
    1. Implemented the uploadFile function.

---
+++ src/file/file.repository.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service.js";
+import type UploadFileDto from "./file-dto/upload-file.dto.js";
+import type UpdateFileDto from "./file-dto/update-file.dto.js";
+
+@Injectable()
+class FileRepository {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async uploadFile(
+        uploadFileDto: UploadFileDto,
+        fileBuffer: Buffer,
+    ) {
+        const arrayBuffer = new ArrayBuffer(fileBuffer.byteLength);
+        const uint8Array = new Uint8Array(arrayBuffer);
+
+        uint8Array.set(fileBuffer);
+
+        return this.prisma.file.create({
+            data: {
+                fileName: uploadFileDto.fileName,
+                fileSize: uploadFileDto.fileSize,
+                fileType: uploadFileDto.fileType,
+                buffer: uint8Array,
+                uploadedBy: uploadFileDto.uploadedBy,
+            },
+        });
+    }
+}
+
+export default FileRepository;
