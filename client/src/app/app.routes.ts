746d9c0ef91be03c88aedddd68155cd3dfaddfbd
Niroj Shah <nirojshah1998@gmail.com>
2026-09-22 23:27:00 +0530

    Define the route to the home page.
    and other routes should redirect to login page.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './app/page/home/home';
+import { Login } from './app/page/login/login';
+import { Signup } from './app/page/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: '**', redirectTo: '/login' },
+];
