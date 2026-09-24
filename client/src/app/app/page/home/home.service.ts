8fee7822dff9218de9c2f3f75e6cfbc1453e3e63
Niroj Shah <nirojshah1998@gmail.com>
2026-09-24 22:59:29 +0530

    feat(home): implement file fetching service
    * Implement fetchFiles API integration
    * Add service methods for file count and recent files
    * Add file upload service method structure

--- client/src/app/app/page/home/home.service.ts
+++ client/src/app/app/page/home/home.service.ts
@@ -8,13 +8,26 @@ interface IHomeService {
     uploadFile(file: File): Observable<any>;
 }
 
-class HomeService {
+class HomeService implements IHomeService {
 
     private readonly url = "http://localhost:5005/api/v1/file";
 
     constructor(private readonly http: HttpClient) { }
 
+    fetchFiles(): Observable<any> {
+        return this.http.get<any>(`${this.url}/files`);
+    }
 
+    fetchNoOfFiles(): Observable<number> {
+
+    }
+
+    fetchRecentFiles(): Observable<any> {
+
+    }
+
+    uploadFile(file: File): Observable<any> {
+    }
 }
 
 export default HomeService;
