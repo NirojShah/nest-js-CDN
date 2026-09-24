361683fa37779969663744d5c250656ca3d310b1
Niroj Shah <nirojshah1998@gmail.com>
2026-09-24 22:58:36 +0530

    feat(home): initialize home service structure
    * Add HomeService with API base URL
    * Define file-related service methods
    * Add IHomeService interface for home operations
    * Prepare RxJS imports for API responses

--- client/src/app/app/page/home/home.service.ts
+++ client/src/app/app/page/home/home.service.ts
@@ -7,3 +7,14 @@ interface IHomeService {
     fetchRecentFiles(): Observable<any>;
     uploadFile(file: File): Observable<any>;
 }
+
+class HomeService {
+
+    private readonly url = "http://localhost:5005/api/v1/file";
+
+    constructor(private readonly http: HttpClient) { }
+
+
+}
+
+export default HomeService;
