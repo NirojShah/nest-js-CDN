204dab955f4e24b9c174b353fbc20c47e41ae278
Niroj Shah <nirojshah1998@gmail.com>
2026-09-26 16:57:35 +0530

    feat(file): register file and permission providers in the Nest module

--- server/src/file/file.module.ts
+++ server/src/file/file.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller.js';
 import FileServiceImpl from './file.service.js';
-import { UserService } from '../user/user.service.js';
 import FileRepository from './file.repository.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { FilePermissionRepository } from './file-permission.repository.js';
+import { FilePermissionService } from './file-permission.service.js';
 
 @Module({
     imports: [PrismaModule],
     controllers: [FileController],
-    providers: [FileServiceImpl, FileRepository],
-    exports: [FileServiceImpl],
+    providers: [FileServiceImpl, FileRepository, FilePermissionRepository, FilePermissionService],
+    exports: [FileServiceImpl, FilePermissionService],
 })
 export class FileModule { }
